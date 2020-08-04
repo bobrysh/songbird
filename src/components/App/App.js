80b144904f4a870fb0eb1b83fd224f43149bcb2e
Pavel Bobryshev <bobr.8590@gmail.com>
2020-08-04 14:24:22 +0300

    song bird init

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+import './App.scss';
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <Container />
+      </>
+    );
+  }
+}
